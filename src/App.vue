b4bbf9d54ff384a6ec4895a3a08f401644b6087b
Lazizbek <lazizabdullayev1987@gmail.com>
2023-02-14 12:14:56 +0500

    Add vuex, tailwindcss & heroicons

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script>
+import { BeakerIcon } from '@heroicons/vue/24/solid'
+export default {
+    components: {BeakerIcon}
+}
 </script>
 
 <template>
-    <h1>Hello</h1>
+    <h1 class="text-blue-500">Hello</h1>
+    <BeakerIcon class="w-6 h-6"/>
 </template>
 
 <style scoped>
+
 </style>
